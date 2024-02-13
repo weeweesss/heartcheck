4c5d0168a7dff8a19ac19efe59eb7fd5d02f157c
weeweesss <158825434+weeweesss@users.noreply.github.com>
2024-02-13 19:23:06 +0500

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,4 +1,4 @@
-# напиши здесь код основного приложения и первого экрана# напиши здесь код основного приложения и первого экрана
+# напиши здесь код основного приложения и первого экрана
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
 

ff78cb764daef87334f32de47db22a488e51bd3d
weeweesss <158825434+weeweesss@users.noreply.github.com>
2024-02-18 13:57:03 +0500

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -1,25 +1,79 @@
+# напиши здесь код третьего экрана приложения# напиши здесь код третьего экрана приложения
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit
 
 from instr import *  #загружаем переменные из файла instr.py
 
 class FinalWin(QWidget):
-    def __init__(self):
+    def __init__(self, exp):
         super().__init__()
+        #получаем данные об эксперименте
+        self.exp = exp
         self.set_appear() # устанавливает, как будет выглядеть окно
         self.initUI() # создаём и настраиваем графические элементы
         self.show()  # показываем окно
-    def set_appear(self):
-        self.setWindowTitle(txt_title)
-        self.resize(win_width, win_height)
-        self.move(win_x, win_y)
-    def initUI(self):
-        self.layout = QVBoxLayout()
-        self.index_text = QLabel(txt_index)
-        self.workheart_text = QLabel(txt_workheart)
-        self.layout.addWidget(self.index_text)
-        self.layout.addWidget(self.workheart_text)
-        self.setLayout(self.layout)
+    
+    def results(self): # возвращает результат измерения в виде строки
+        self.index=(4*(int(self.exp.test1)+int(self.exp.test2)+int(self.exp.test3))-200)/10
+        if int(self.exp.age) < 7:
+            #self.index = 0
+            return "нет данных для такого возраста"
+        if int(self.exp.age) == 7 or int(self.exp.age) == 8:
+            if self.index >= 21:
+                return txt_res1
+            elif self.index < 21 and self.index >= 17:
+                return txt_res2
+            elif self.index < 17 and self.index >= 12:
+                return txt_res3
+            elif self.index < 12 and self.index >= 6.5:
+                return txt_res4
+            else:
+                return txt_res5
+        if int(self.exp.age) == 9 or int(self.exp.age) == 10:
+            if self.index >= 19.5:
+                return txt_res1
+            elif self.index < 19.5 and self.index >= 15.5:
+                return txt_res2
+            elif self.index < 15.5 and self.index >= 10.5:
+                return txt_res3
+            elif self.index < 10.5 and self.index >= 5:
+                return txt_res4
+            else:
+                return txt_res5
+        if int(self.exp.age) == 11 or int(self.exp.age) == 12:
+            if self.index >= 18:
+                return txt_res1
+            elif self.index < 18 and self.index >= 14:
+                return txt_res2
+            elif self.index < 14 and self.index >= 9:
+                return txt_res3
+            elif self.index < 9 and self.index >= 3.5:
+                return txt_res4
+            else:
+                return txt_res5
+        if int(self.exp.age) == 13 or int(self.exp.age) == 14:
+            if self.index >= 16.5:
+                return txt_res1
+            elif self.index < 16.5 and self.index >= 12.5:
+                return txt_res2
+            elif self.index < 12.5 and self.index >= 7.5:
+                return txt_res3
+            elif self.index < 7.5 and self.index >= 2:
+                return txt_res4
+            else:
+                return txt_res5
+        if int(self.exp.age) >= 15:
+            if self.index >= 15:
+                return txt_res1
+            elif self.index < 15 and self.index >= 11:
+                return txt_res2
+            elif self.index < 11 and self.index >= 6:
+                return txt_res3
+            elif self.index < 6 and self.index >= 0.5:
+                return txt_res4
+            else:
+                return txt_res5
+
     def initUI(self):
         ''' создает графические элементы '''
         self.workh_text = QLabel(txt_workheart + self.results())
